bc429598162356913cdba10cfa36c725c2d690ce
Noobysaibots <74640216+Evelina704@users.noreply.github.com>
2026-01-14 22:33:16 +0600

    Refactor error message formatting for clarity

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -15,7 +15,9 @@ def run_game(game):
         answer = prompt.string('Your answer: ')
 
         if answer != str(correct_answer):
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(
+                f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."
+            )
             print(f"Let's try again, {name}!")
             return
 
