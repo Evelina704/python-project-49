41ba99d8f1b8ca835b7af7c85b0d5c89dab4b711
Noobysaibots <74640216+Evelina704@users.noreply.github.com>
2026-01-14 22:38:40 +0600

    Fix error message formatting in engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -16,7 +16,8 @@ def run_game(game):
 
         if answer != str(correct_answer):
             print(
-                f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'."
+                f"'{answer}' is wrong answer ;(. "
+                f"Correct answer was '{correct_answer}'."
             )
             print(f"Let's try again, {name}!")
             return
