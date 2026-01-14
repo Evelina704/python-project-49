ec5b1101ea89bc61dc5fba4e72aee7853f450a5f
Noobysaibots <74640216+Evelina704@users.noreply.github.com>
2026-01-14 22:11:45 +0600

    Implement brain progression game logic

---
+++ brain_games/games/brain_progression.py
@@ -0,0 +1,17 @@
+import random
+
+RULE = 'What number is missing in the progression?'
+
+
+def generate_round():
+    length = random.randint(5, 10)
+    start = random.randint(1, 20)
+    step = random.randint(1, 10)
+
+    progression = [start + step * i for i in range(length)]
+    hidden_index = random.randint(0, length - 1)
+    correct_answer = str(progression[hidden_index])
+
+    progression[hidden_index] = '..'
+    question = ' '.join(map(str, progression))
+    return question, correct_answer
